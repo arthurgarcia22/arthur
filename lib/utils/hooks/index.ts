ea6d67042277dfd2749d3a6fd47763a00d5190d9
luizxrs <lgustavoitp@gmail.com>
2024-07-01 22:58:37 -0300

    removed unused code

--- lib/utils/hooks/index.ts
+++ lib/utils/hooks/index.ts
@@ -1,8 +1,5 @@
-export { useCalcState } from "./useCalcState";
-export { useInViewPort } from "./useInViewPort";
 // export { useMapBox } from "./useMapBox";
 export { useMobileDetect } from "./useMobileDetect";
 export * from "./useMousePosition";
 export { useScrollPos } from "./useScrollPos";
-export { useWindowDimensions } from "./useWindowDimensions";
 export { useWindowSize } from "./useWindowSize";
